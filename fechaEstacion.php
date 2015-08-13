7f3579de70310fc11563c84836478a1ebc9f306a
Rocío Mariel Gnas <rociomarielgnas@gmail.com>
2015-08-13 10:47:49 -0300

    Ahora funciona el switch y muestra la estación

--- fechaEstacion.php
+++ fechaEstacion.php
@@ -13,41 +13,73 @@ $mes = date("m");
 switch($mes){
 
 	case "01":
-		print "${dia}/${mes}: Estás en verano.";
+		print "${dia}/${mes}: Estás en Verano.";
 	break;
 	
 	case "02":
-		print "${dia}/${mes}: Estás en verano.";
+		print "${dia}/${mes}: Estás en Verano.";
 	break;
 
 	case "03":
-		if($dia < 21)
+		if ($dia < 21) {
+			print "${dia}/${mes}: Estás en Verano.";	
+		}
+		else {
+			print "${dia}/${mes}: Estás en Otoño.";
+		}
 	break;	
-	
+		
 	case "04":
+		print "${dia}/${mes}: Estás en Otoño.";
 	break;	
 	
 	case "05":
+		print "${dia}/${mes}: Estás en Otoño.";
 	break;	
 	
 	case "06":
+		print "${dia}/${mes}: Estás en Otoño.";
 	break;	
 	
 	case "07":
+		if ($dia < 21) {
+			print "${dia}/${mes}: Estás en Otoño.";	
+		}
+		else {
+			print "${dia}/${mes}: Estás en Invierno.";
+		}
 	break;	
 	
 	case "08":
+		print "${dia}/${mes}: Estás en Invierno.";
 	break;	
 	
 	case "09":
+		if ($dia < 21) {
+			print "${dia}/${mes}: Estás en Invierno.";	
+		}
+		else {
+			print "${dia}/${mes}: Estás en Primavera.";
+		}
 	break;	
 	
 	case "10":
+		print "${dia}/${mes}: Estás en Primavera.";
 	break;	
-	case "11":
-	break;	
-	case "12":
-	break;
 
+	case "11":
+		print "${dia}/${mes}: Estás en Primavera.";
+	break;	
+	
+	case "12":
+		if ($dia < 21) {
+			print "${dia}/${mes}: Estás en Primavera";	
+		}
+		else {
+			print "${dia}/${mes}: Estás en Verano.";
+		}	
+	break;
 }
 ?>
+
+	
