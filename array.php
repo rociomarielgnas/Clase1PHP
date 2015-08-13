d8ce914037b8f709f2c5575d08ed49406e61370a
Rocío Mariel Gnas <rociomarielgnas@gmail.com>
2015-08-13 15:16:52 -0300

    Ahora cambia el formato del texto
    Los formatos también los toma de un array.

--- array.php
+++ array.php
@@ -1,9 +1,13 @@
 <?php
 
 $colores = array("IndianRed", "DarkOrange", "#FFFF00", "Indigo", "#7CFC00");
+$formatos = array("b", "i", "u", "strike", "b");
+$j = 0;
+
 $count = count($colores);
 
 for($i = 0; $i < $count; $i++){
-	echo ("<p style='color:$colores[$i]'>What</p>");
+	echo ("<p style='color:$colores[$i]'><$formatos[$j]>What</$formatos[$j]></p>");
+	$j++;
 }
 ?>
