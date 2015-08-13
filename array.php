486c1e63e23875248250618c09f4d5a5b5fe4410
Rocío Mariel Gnas <rociomarielgnas@gmail.com>
2015-08-13 15:02:33 -0300

    Actualicé array.php
    Ahora sí anda.

--- array.php
+++ array.php
@@ -4,7 +4,6 @@ $colores = array("IndianRed", "DarkOrange", "#FFFF00", "Indigo", "#7CFC00");
 $count = count($colores);
 
 for($i = 0; $i < $count; $i++){
-	echo ("<p><font color=&#34".$colores[$i]."&#34>Lorem ipsum dolor sit amet.</font></p>");
+	echo ("<p style='color:$colores[$i]'>What</p>");
 }
-
 ?>
